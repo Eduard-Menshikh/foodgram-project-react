b3a97cbaf95918e612f3883aacc3c5434f272e70
Эдик Меньших <ferdferdferdyy@gmail.com>
2023-08-12 19:03:28 +0300

    3rd try

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -84,10 +84,6 @@ DATABASES = {
 }
 
 
-# Password validation
-# https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
-
-AUTH_USER_MODEL = 'recipes.User'
 
 AUTH_PASSWORD_VALIDATORS = [
     {
