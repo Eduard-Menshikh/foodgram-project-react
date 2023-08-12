771c0a972eaf7bc607e393233106712bb3dd9970
Эдик Меньших <ferdferdferdyy@gmail.com>
2023-08-12 18:04:09 +0300

    3rd try

--- backend/recipes/management/commands/importtags.py
+++ backend/recipes/management/commands/importtags.py
@@ -6,5 +6,6 @@ from recipes.models import Tag
 class Command(BaseCommand, CreateAPIView):
     def handle(self, *args, **options):
         Tag.objects.get_or_create(name='Ужин', color='blue', slug='dinner')
-        Tag.objects.get_or_create(name='Завтрак', color='red', slug='breakfast')
+        Tag.objects.get_or_create(name='Завтрак', color='red',
+                                  slug='breakfast')
         Tag.objects.get_or_create(name='обед', color='green', slug='lunch')
