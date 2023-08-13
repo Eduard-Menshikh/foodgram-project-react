03aa42dc00aff9def4d0e991841526b625765b31
Эдик Меньших <ferdferdferdyy@gmail.com>
2023-08-13 22:09:20 +0300

    fix model

--- backend/api/views.py
+++ backend/api/views.py
@@ -5,9 +5,6 @@ from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.decorators import action
-from rest_framework.response import Response
-from rest_framework import status
-from django.shortcuts import get_object_or_404
 
 from recipes.models import (
     Tag, Recipe, Ingredient, Favorite,
@@ -21,6 +18,7 @@ from api.filters import SearchIngredientFilter, RecipeFilter
 from users.models import User, Subscribe
 from api.permissions import IsAuthorOrAdminOrReadOnly
 from rest_framework.generics import CreateAPIView, DestroyAPIView
+from api.mixins import CreateDeleteMixin
 
 
 class CastomUserViewSet(CreateAPIView, DestroyAPIView, UserViewSet):
@@ -69,7 +67,7 @@ class TagViewSet(ModelViewSet):
     pagination_class = None
 
 
-class RecipeViewSet(ModelViewSet):
+class RecipeViewSet(ModelViewSet, CreateDeleteMixin):
     queryset = Recipe.objects.all()
     serializer_class = RecipeCreateSerializer
     permission_classes = (IsAuthorOrAdminOrReadOnly,)
@@ -79,44 +77,24 @@ class RecipeViewSet(ModelViewSet):
     @action(detail=True,
             methods=['post'],
             permission_classes=(IsAuthenticated,))
-    def favorite(self, request, pk, **kwargs):
-        try:
-            recipe = get_object_or_404(Recipe, id=pk)
-            favorite = Favorite.objects.create(user=request.user,
-                                               recipe=recipe)
-            serializer = FavoriteSerializer(favorite)
-        except Recipe.DoesNotExist:
-            return Response({'error': 'Recipe not found.'},
-                            status=status.HTTP_404_NOT_FOUND)
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    def favorite(self, request, pk):
+        data = {'user': request.user.id, 'recipe': pk}
+        return self.create_obj(FavoriteSerializer, data, request)
 
     @favorite.mapping.delete
-    def unfavorite(self, request, pk, **kwargs):
-        try:
-            recipe = get_object_or_404(Recipe, id=pk)
-            favorite = Favorite.objects.delete(user=request.user,
-                                               recipe=recipe)
-            serializer = FavoriteSerializer(favorite)
-        except Recipe.DoesNotExist:
-            return Response({'error': 'Recipe not found.'},
-                            status=status.HTTP_404_NOT_FOUND)
-        return Response(serializer.data, status=status.HTTP_204_NO_CONTENT)
+    def unfavorite(self, request, pk):
+        return self.delete_obj(Favorite, user=request.user, recipe=pk)
 
     @action(detail=True,
             methods=['post'],
             permission_classes=(IsAuthenticated,))
     def shopping_cart(self, request, pk):
         data = {'user': request.user.id, 'recipe': pk}
-        serializer = ShoppingCartSerializer(data=data,
-                                            context={'request': request})
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(status=status.HTTP_201_CREATED)
+        return self.create_obj(ShoppingCartSerializer, data, pk)
 
     @shopping_cart.mapping.delete
     def remove_from_cart(self, request, pk):
-        get_object_or_404(ShoppingCart, user=request.user, recipe=pk).delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+        return self.delete_obj(ShoppingCart, user=request.user, recipe=pk)
 
     @action(detail=False,
             methods=['get'],
