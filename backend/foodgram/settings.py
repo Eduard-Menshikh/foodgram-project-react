f6fbe44d6ee787e3dca7342be0bea83378389034
Эдик Меньших <ferdferdferdyy@gmail.com>
2023-08-14 21:51:53 +0300

    related_name tags

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -13,7 +13,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY')
 
-DEBUG = bool(strtobool(os.getenv('DEBUG', 'False')))
+DEBUG = bool(strtobool(os.getenv('DEBUG', 'True')))
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
 
